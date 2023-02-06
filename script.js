c87859c71bcdf4c2099a34ec3fb78a3802db6ba2
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-02-06 12:07:30 -0700

    working on getting data to display

--- script.js
+++ script.js
@@ -4,12 +4,11 @@ var cityInputEl = document.querySelector('#cityName');
 var citySubmitButtonEl = document.querySelector('citySubmit');
 
 // variables for weather information
+var searchTermEl = document.getElementById ("#search-term")
 var dateEL = document.getElementById('date');
-var currentWeatherItemsEl = document.querySelector('currentWeatherItems')
 var containerEl = document.querySelector('container');
 
 // variables for search history 
-var searchTerm = document.querySelector('search-term');
 var futureForecastEL = document.querySelector('future-forecast');
 
 // event listeners
@@ -32,6 +31,18 @@ function formSubmitHandler(event) {
     }
   };
 
+//Display city name on page
+// document.querySelector("#search-term").textContent = cityName.toUpperCase();
+
+// var buttonClickHandler = function (event) {
+//   var prevCity = event.target.getAttribute('first-city');
+
+//   if (prevCity) {
+//     getStoredData(prevCity);
+
+//     containerEl.textContent = '';
+//   }
+// };
 
 // function to call coordinates and then initiate function to display current weather
 function getCoordinates (cityName) {
@@ -55,16 +66,24 @@ function getCoordinates (cityName) {
     
 
 //get the weather forecast based on city latitude & longitude
-function getWeatherForecast(lat, lon) {
-  
+function getWeatherForecast(lat, lon) { 
   var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac721f3e341fb446253df6241582894c";
-
     console.log();
 
     fetch(forecastUrl)
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
+            var date = data['dt'];
+            var temp = data['main']['temp'] + "F";
+            var wind = data['wind']['speed'] + "MPH";
+            var humid = data['main']['humidity'] + "%";
+
+            date.innerHTML = nameValue;
+            temp.innerHTML = tempValue;
+            wind.innerHTML = windValue;
+            humid.innerHTML = humidityValue;
+
             console.log(data);
             // showWeatherForecast();
           });
@@ -72,33 +91,53 @@ function getWeatherForecast(lat, lon) {
           console.log('Api is not working')
         }
       })
-    }
-          
-function showWeatherForecast() {
+    
 
+  //  //display current date
+  // var currentDate = document.querySelector("#current-date")
+  // currentDate.textContent = currentDate;
+  // currentDate.style.display = "block";
+
+// This strategy for displaying the data was inspired by this youtube: https://www.youtube.com/watch?v=Mc1w6Q-nxzM
+// function showWeatherForecast(data) {
+//   var currentWeatherItemsEl = document.querySelector("currentWeatherItems");
+
+
+  // var date = document.createElement("div");
+  // date.textContent = "Date" = data.dt;
+  // currentWeatherItemsEl.append(date);
+
+  // var temp = document.createElement("div");
+  // temp.textContent = "Temp: " + data.main.temp + "F";
+  // currentWeatherItemsEl.append(temp);
+
+  // var wind = document.createElement("div");
+  // wind.textContent = "Wind " + data.wind_speed;
+  // currentWeatherItemsEl.append(wind);
+
+  // var humid = document.createElement("div");
+  // humid.textContent = "Humidity: " + data.main.humidity + "%";
+  // currentWeatherItemsEl.append(humid);
 
 }
+
 // var showWeather = function (data, searchTerm) {
 //   if (weather.length === 0) {
 //     containerEl.textContent = 'No city weather forecast found.';
 //     return;
 //   }
+// }
 
-// var buttonClickHandler = function (event) {
-//   var prevCity = event.target.getAttribute('first-city');
 
-//   if (prevCity) {
-//     getStoredData(prevCity);
-
-//     containerEl.textContent = '';
+//display the weather icons on the page
+// function getIcons(weatherData) {
+//   for (let i = 0; i < 6; i++) {
+//     weatherIcon = weatherData[i].weather[0].icon;
+//     iconElement = document.querySelector(`#icon-${i}`);
+//     iconElement.src =
+//       "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
 //   }
-// };
-
-
-
-
-
-//   repoSearchTerm.textContent = searchTerm;
+// }
 
 //   for (var i = 0; i < repos.length; i++) {
 //     var repoName = repos[i].owner.login + '/' + repos[i].name;
