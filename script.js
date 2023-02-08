cefec212f28406b9b97b33aae0622f3c3998b937
Scarlet234 <109751916+Scarlet234@users.noreply.github.com>
2023-02-07 19:14:00 -0700

    5-day weather cards coded

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function formSubmitHandler(event) {
       return;
       // window.alert('Please enter a city name');
     }
+
   };
 
-//Display city name on page
-// document.querySelector("#search-term").textContent = cityName.toUpperCase();
+// Display city name on page
 
 // var buttonClickHandler = function (event) {
 //   var prevCity = event.target.getAttribute('first-city');
@@ -61,25 +61,26 @@ function getCoordinates (cityName) {
           console.log('Api is not working')
         }
       })
-      
+      document.querySelector("#search-term").textContent = cityName;
     }
     
 
 //get the weather forecast based on city latitude & longitude
 function getWeatherForecast(lat, lon) { 
-  var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=ac721f3e341fb446253df6241582894c";
-    console.log();
+  var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?&units=imperial&lat=" + lat + "&lon=" + lon + "&appid=ac721f3e341fb446253df6241582894c";
+    
 
     fetch(forecastUrl)
       .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
-            console.log(data);
+           
             var temp = data.list[0].main.temp + "F";
             var wind = data.list[0].wind.speed + "MPH";
             var humid = data.list[0].main.humidity + "%";
-          console.log(temp);
-          showWeatherForecast(temp, wind, humid);
+            var dates = [data.list[0], data.list[8], data.list[16], data.list[24], data.list[32]]
+          showCurrentWeatherForecast(temp, wind, humid);
+          showFutureWeather(dates);
           });
         } else {
           console.log('Api is not working')
@@ -90,12 +91,11 @@ function getWeatherForecast(lat, lon) {
             
 
 // This strategy for displaying the data was inspired by this youtube: https://www.youtube.com/watch?v=Mc1w6Q-nxzM
-function showWeatherForecast(temp, wind, humid) {
-  var currentWeatherItemsEl = document.getElementById("#currentContainer");
-  // var date = data.list[0].clouds.dt_txt;
-  // var temp = data.list[0].main.temp + "F";
-  // var wind = data.list[0].wind.speed + "MPH";
-  // var humid = data.list[0].main.humidity + "%";
+function showCurrentWeatherForecast(temp, wind, humid) {
+  // var citySearch = document.querySelector("#search-term");
+  var currentTempEl = document.querySelector("#temp");
+  var currentWindEl = document.querySelector("#wind");
+  var currentHumidEl = document.querySelector("#humid");
   
   // var day = document.createElement('p');
   // day.textContent = date;
@@ -103,27 +103,47 @@ function showWeatherForecast(temp, wind, humid) {
 
   var temperature = document.createElement('p');
   temperature.textContent = temp;
-  currentWeatherItemsEl.append(temperature);
+  currentTempEl.appendChild(temperature);
 
   var windspeed = document.createElement('p');
   windspeed.textContent = wind;
-  currentWeatherItemsEl.append(windspeed);
+  currentWindEl.appendChild(windspeed);
 
   var humidity = document.createElement('p');
   humidity.textContent = humid;
-  currentWeatherItemsEl.append(humidity);
+  currentHumidEl.appendChild(humidity);
+}
+
+function showFutureWeather(dates) {
+  for (var i = 0; i < dates.length; i++) {
+    console.log(dates[i])
+    var temp = dates[i].main.temp + "F";
+    var wind = dates[i].wind.speed + "MPH";
+    var humid = dates[i].main.humidity + "%";
+    var futureWeather = document.querySelector("#futureContainer");
+    var weatherContainer = document.createElement('div');
+    var temperature = document.createElement('p');
+    temperature.textContent = temp;
+    weatherContainer.append(temperature)
+    var windspeed = document.createElement('p');
+    windspeed.textContent = wind;
+    weatherContainer.append(windspeed)
+ 
+   var humidity = document.createElement('p');
+    humidity.textContent = humid;
+    weatherContainer.append(humidity)
+
+    futureWeather.append(weatherContainer);
+
+    weatherContainer.setAttribute("class", "weatherContainer")
+  }
 
 }
 
-// var showWeather = function (data, searchTerm) {
-//   if (weather.length === 0) {
-//     containerEl.textContent = 'No city weather forecast found.';
-//     return;
-//   }
-// }
 
 
-//display the weather icons on the page
+
+//display the weather icons on the page--example
 // function getIcons(weatherData) {
 //   for (let i = 0; i < 6; i++) {
 //     weatherIcon = weatherData[i].weather[0].icon;
@@ -133,19 +153,5 @@ function showWeatherForecast(temp, wind, humid) {
 //   }
 // }
 
-//   for (var i = 0; i < repos.length; i++) {
-//     var repoName = repos[i].owner.login + '/' + repos[i].name;
 
-//     var repoEl = document.createElement('a');
-//     repoEl.classList = 'list-item flex-row justify-space-between align-center';
-//     repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-//     var titleEl = document.createElement('span');
-//     titleEl.textContent = repoName;
-
-//     repoEl.appendChild(titleEl);
-
-//     repoContainerEl.appendChild(repoEl);
-//   }
-// };
 
